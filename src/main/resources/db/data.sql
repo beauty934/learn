411b1521de963c640315f7d552a4e002aa2aeeed
beauty934 <2656266096@qq.com>
2019-10-12 19:40:54 +0800

    update 1012

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -7,4 +7,5 @@ INSERT INTO `user` (id, username, password, role) VALUES ('6', 'user4', '1234', 
 
 
 INSERT INTO `music`(song_name, singer, release_date)VALUES('说好不哭','周杰伦','***');
+INSERT INTO `music`(song_name, singer, release_date)VALUES('遇见','孙燕姿','***');
 COMMIT;
